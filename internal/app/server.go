06ebad3c0c8eeb00aa0d805cf9950a8f8bf19d94
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-01-12 10:16:55 -0500

    Create a server handler

---
+++ internal/app/server.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/teragrammer/payment-gateway-wrapper/internal/database"
+)
+
+func Run(handler http.Handler, port string) {
+	srv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	defer database.CloseMongo()
+	defer database.CloseRedis()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		return
+	}
+}

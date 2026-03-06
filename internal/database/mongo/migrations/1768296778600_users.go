bd6b318c5ddd58f7fefad774570dc86d0e182df8
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-03-06 09:44:31 -0500

    Create migration for users

---
+++ internal/database/mongo/migrations/1768296778600_users.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"context"
+	"time"
+
+	"github.com/teragrammer/payment-gateway-wrapper/internal/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func UsersCollection(db *mongo.Database) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	collectionName := "users"
+
+	err := utils.CreateCollection(collectionName, db, ctx)
+	if err != nil {
+		return err
+	}
+
+	indexes := []mongo.IndexModel{
+		{
+			Keys: bson.D{{Key: "username", Value: -1}},
+			Options: options.Index().
+				SetUnique(true).
+				SetName("uniq_username"),
+		},
+		{
+			Keys: bson.D{{Key: "created_at", Value: -1}},
+			Options: options.Index().
+				SetName("created_at_idx"),
+		},
+	}
+
+	_, err = db.Collection(collectionName).Indexes().CreateMany(ctx, indexes)
+	return err
+}

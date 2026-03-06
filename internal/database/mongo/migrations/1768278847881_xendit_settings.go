3712822222e1c710d8e3e4e983feb87437301c96
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-03-06 09:44:10 -0500

    Create migration for Xendit settings
    Add: migration model

---
+++ internal/database/mongo/migrations/1768278847881_xendit_settings.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"context"
+	"time"
+
+	"github.com/teragrammer/payment-gateway-wrapper/internal/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func XenditSettingsCollection(db *mongo.Database) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	collectionName := "xendit_settings"
+
+	err := utils.CreateCollection(collectionName, db, ctx)
+	if err != nil {
+		return err
+	}
+
+	indexes := []mongo.IndexModel{
+		{
+			Keys: bson.D{{Key: "project_id", Value: 1}},
+			Options: options.Index().
+				SetName("project_id_idx"),
+		},
+		{
+			Keys: bson.D{{Key: "slug", Value: 1}},
+			Options: options.Index().
+				SetUnique(true).
+				SetName("uniq_slug"),
+		},
+		{
+			Keys: bson.D{{Key: "created_at", Value: -1}},
+			Options: options.Index().
+				SetName("created_at_idx"),
+		},
+	}
+
+	_, err = db.Collection(collectionName).Indexes().CreateMany(ctx, indexes)
+	return err
+}

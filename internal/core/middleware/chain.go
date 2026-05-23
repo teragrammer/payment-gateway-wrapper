d27fb676d5deecc3f92f10e1cfe5017c784c046d
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-05-23 11:05:16 -0400

    feat: add reusable MongoDB cursor query utility

---
+++ internal/core/middleware/chain.go
@@ -0,0 +1,12 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.Handler) http.Handler
+
+func Chain(h http.Handler, m ...Middleware) http.Handler {
+	for i := len(m) - 1; i >= 0; i-- {
+		h = m[i](h)
+	}
+	return h
+}

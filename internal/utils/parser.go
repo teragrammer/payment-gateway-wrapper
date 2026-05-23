472db8069210973177edba40570db79d3d471e02
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-05-23 11:07:06 -0400

    feat(helper): add IntToString and StringToObjectID helpers

--- internal/utils/parser.go
+++ internal/utils/parser.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"strconv"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // StringToInt converts a string to an integer and returns an error if the conversion fails.
@@ -22,4 +24,16 @@ func StringToInt64(s string, def int64) int64 {
 		return def
 	}
 	return num
+}
+
+func IntToString(num int) string {
+	return strconv.Itoa(num) // Use strconv.Itoa to convert int to string
+}
+
+func StringToObjectID(s string) primitive.ObjectID {
+	objectID, err := primitive.ObjectIDFromHex(s)
+	if err != nil {
+		return primitive.NilObjectID
+	}
+	return objectID
 }
